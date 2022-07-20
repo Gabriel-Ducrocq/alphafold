5d5eaac94c9235a3a28f301e813b175b0af8f9e9
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2022-07-20 17:57:03 +0200

    again

--- alphafold/data/templates.py
+++ alphafold/data/templates.py
@@ -565,6 +565,10 @@ def _extract_template_features(
   try:
     # Essentially set to infinity - we don't want to reject templates unless
     # they're really really bad.
+    print("chain_id", chain_id)
+    print("MMCIF object")
+    print(mmcif_object)
+    print("\n\n\n\n\n\n\n\n\n")
     all_atom_positions, all_atom_mask = _get_atom_positions(
         mmcif_object, chain_id, max_ca_ca_distance=150.0)
   except (CaDistanceError, KeyError) as ex:
