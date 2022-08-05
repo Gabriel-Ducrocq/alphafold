0ddf7ff4bcd1f50729433be43d7c76cb7e4aab94
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2022-08-05 12:04:18 +0200

    modifying the chain cleaner to account for several same identifiers in different chains like A1 and A in the output of phenix

--- alphafold/data/pdb_cleaner.py
+++ alphafold/data/pdb_cleaner.py
@@ -1,5 +1,7 @@
 from absl import flags
 from absl import app
+import numpy as np
+import string
 
 flags.DEFINE_string('pdb_path_input', None, 'Path to the pdb file to clean.')
 flags.DEFINE_string('pdb_path_output', None, 'Path to the pdb file to the cleaned.')
@@ -9,14 +11,32 @@ def main(argv):
     with open(FLAGS.pdb_path_input, "r") as f:
         handle = f.readlines()
 
-    all_lines = []
+    unique_identifier = []
+    nb_TER = 0
     for line in handle:
         if "ATOM" in line:
             if line[20] != ' ':
                 line_list = list(line)
-                line_list[21] = line_list[20]
-                line_list[20] = ' '
-                line = ''.join(line_list)
+                unique_identifier.append(line_list[20])
+            else:
+                line_list = list(line)
+                unique_identifier.append(line_list[21])
+
+        if line.strip("\n") == "TER":
+            nb_TER += 1
+
+    chain_number = 0
+    all_lines = []
+    for line in handle:
+        if "ATOM" in line:
+            line_list = list(line)
+            line_list[21] = string.ascii_uppercase[chain_number]
+            line_list[20] = ' '
+            line = ''.join(line_list)
+
+        elif line.strip("\n") == "TER":
+            chain_number += 1
+
 
         all_lines.append(line)
 
